861ff01f1615796a96b7466f911a50c623b435d0
Ryan Schmukler <rschmukler@gmail.com>
2015-07-15 00:22:19 -0400

    wip commit

--- Dockerfile
+++ Dockerfile
@@ -1,20 +1,28 @@
 FROM ubuntu:14.04
 
+# Flocker Agent
 RUN apt-get -y install apt-transport-https software-properties-common && \
     add-apt-repository -y ppa:james-page/docker && \
     add-apt-repository -y "deb https://clusterhq-archive.s3.amazonaws.com/ubuntu/$(lsb_release --release --short)/\$(ARCH) /" && \
     apt-get update && \
-    apt-get -y --force-yes install clusterhq-flocker-node
+    apt-get -y --force-yes install clusterhq-flocker-node 
+
+# Flocker-Docker-Plugin
+RUN apt-get -y install python-pip build-essential libssl-dev libffi-dev python-dev wget && \
+    pip install git+https://github.com/clusterhq/flocker-docker-plugin.git
 
 RUN mkdir /etc/flocker
+RUN groupadd docker
+ADD build/docker /usr/bin/docker
 ADD start.sh /root/flocker-config/start.sh
-ADD /agents/base-agent.yml /root/flocker-config/base-agent.yml
-ADD /agents/aws-agent.yml /root/flocker-config/aws-agent.yml
+ADD build/agents/base-agent.yml /root/flocker-config/base-agent.yml
+ADD build/agents/aws-agent.yml /root/flocker-config/aws-agent.yml
+ADD build/ca-certificates.crt /etc/ssl/certs/
 
-EXPOSE 5423
-EXPOSE 5424
+EXPOSE 4523
+EXPOSE 4524
 
-VOLUME [ "/etc/flocker" ]
+VOLUME [ "/etc/flocker", "/flocker" ]
 
 WORKDIR /root/flocker-config
 
--- start.sh
+++ start.sh
@@ -11,6 +11,7 @@
 # FLOCKER_DATASET_AWS_SECRET
 
 FLOCKER_CONTROL_MODE=${FLOCKER_CONTROL_MODE:-false}
+FLOCKER_CONTROL_PORT="${FLOCKER_CONTROL_PORT:-4524}"
 
 CLUSTER_CRT=/etc/flocker/cluster.crt
 CONTROL_CRT=/etc/flocker/control-service.crt
@@ -70,7 +71,6 @@ function buildAgent() {
     echo "No FLOCKER_CONTROL_HOST specified. Exiting..."
     exit 1
   fi
-  FLOCKER_CONTROL_PORT="${FLOCKER_CONTROL_PORT:-4524}"
 
   sed -i 's/CONTROL_HOST/'$FLOCKER_CONTROL_HOST'/' $BASE_FILE
   sed -i 's/CONTROL_PORT/'$FLOCKER_CONTROL_PORT'/' $BASE_FILE
@@ -88,15 +88,66 @@ function buildAgent() {
 }
 
 function runFlocker() {
-  FLOCKER_CTRL_PATH = /usr/sbin/flocker-control
+  echo "Starting Flocker..."
+  FLOCKER_CTRL_PATH=/usr/sbin/flocker-control
+  FLOCKER_DATASET_AGENT_PATH=/usr/sbin/flocker-dataset-agent
+  FLOCKER_CONTAINER_AGENT_PATH=/usr/sbin/flocker-container-agent
   if [ $FLOCKER_CONTROL_MODE = "true" ]; then 
     $FLOCKER_CTRL_PATH -p tcp:4523 -a tcp:$FLOCKER_CONTROL_PORT
   else
-    service flocker-dataset-agent start
-    service flocker-container-agent start
+    $FLOCKER_DATASET_AGENT_PATH &
+    $FLOCKER_CONTAINER_AGENT_PATH &
   fi
+}
+
+function runFlockerPlugin() {
+  echo "Starting Flocker Plugin..."
+  export FLOCKER_CONTROL_SERVICE_BASE_URL=https://${FLOCKER_CONTROL_HOST}:${FLOCKER_CONTROL_PORT}/v1
+  export MY_NETWORK_IDENTITY=`hostname -I | awk '{print $2}'`
+  flocker-docker-plugin &
+}
+
+function startDocker() {
+  echo "Starting Docker..."
+  set -x -e
+
+  TLS_PATH=/etc/docker/tls
+  CGROUPS="perf_event net_cls freezer devices blkio memory cpuacct cpu cpuset"
+
+  mkdir -p /sys/fs/cgroup
+  mount -t tmpfs none /sys/fs/cgroup
+
+  for i in $CGROUPS; do
+      mkdir -p /sys/fs/cgroup/$i
+      mount -t cgroup -o $i none /sys/fs/cgroup/$i
+  done
+
+  if ! lsmod | grep -q br_netfilter; then
+      modprobe br_netfilter 2>/dev/null || true
+  fi
+
+  rm -f /var/run/docker.pid
+
+  ARGS=$(echo $(ros config get user_docker.args | sed 's/^-//'))
+  ARGS="$ARGS $(echo $(ros config get user_docker.extra_args | sed 's/^-//'))"
+
+  if [ "$(ros config get user_docker.tls)" = "true" ]; then
+      ARGS="$ARGS $(echo $(ros config get user_docker.tls_args | sed 's/^-//'))"
+      ros tls generate --server -d $TLS_PATH
+      cd $TLS_PATH
+  fi
+
+  if [ -e /var/lib/rancher/conf/docker ]; then
+      source /var/lib/rancher/conf/docker
+  fi
+
+  exec $ARGS $DOCKER_OPTS >/var/log/docker.log 2>&1
 }
 
 checkCerts
 buildAgent
 runFlocker
+if [ "$FLOCKER_CONTROL_MODE" != "true" ]; then 
+  startDocker
+  runFlockerPlugin
+fi
